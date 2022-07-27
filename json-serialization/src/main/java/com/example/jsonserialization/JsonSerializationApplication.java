de85144ebba4c2af9e0162722b9b988b7cdd5798
wujiawei <wujiawei0926@yeah.net>
2022-07-27 08:39:31 +0800

    JSON序列化

---
+++ json-serialization/src/main/java/com/example/jsonserialization/JsonSerializationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSerializationApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSerializationApplication.class, args);
+    }
+    
+}
---
+++ json-serialization/src/test/java/com/example/jsonserialization/JsonSerializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonserialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonSerializationApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
